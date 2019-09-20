2d5c17b7d56f662cf1aeb93bcd3dd7f5e1bf3347
henry-west <henrywest93@gmail.com>
2019-09-20 15:05:51 -0500

    1 - allow exactly two numbers

---
+++ challenge-calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace challenge_calculator
+{
+    class ChallengeCalculator
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var nums = args[0].Split(",");
+                if (nums.Length == 2)
+                {
+                    Console.WriteLine(nums.Sum(x => Int32.Parse(x)));
+                }
+            }
+        }
+    }
+}
